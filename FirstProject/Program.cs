90ae71446b520f7970578562927f95177914b88f
vitordosantosfernandes@gmail.com <7004181@spdm.org.br>
2026-01-26 12:43:47 -0300

    Configuração inicial do projeto C# e fundamentos

---
+++ FirstProject/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+            int z = 32;
+            char gener = 'F';
+            int a,b;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F4"));
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Resultado = " + x); Console.WriteLine("O valor do troco é " + x + " reais");
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+            Console.WriteLine("A idade da paciente é "+ z + " anos e o sexo dela é "+ gener);
+
+            a = 5;
+            b = 2*a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+        }
+    }
+}
